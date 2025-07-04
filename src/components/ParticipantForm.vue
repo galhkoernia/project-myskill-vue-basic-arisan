55d35e4167e4074d23ff93a4fb4f10d181722295
Galuh Kurnia <galuhkoernia979@gmail.com>
2025-07-04 22:47:41 +0700

    project vue

---
+++ src/components/ParticipantForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <form @submit.prevent="submitForm" class="form-container">
+    <input
+      type="text"
+      v-model="name"
+      placeholder="Masukkan nama peserta"
+      class="input"
+    />
+    <button type="submit" class="button">Tambah</button>
+    <p v-if="error" class="error">{{ error }}</p>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'ParticipantForm',
+  data() {
+    return {
+      name: '',
+      error: ''
+    }
+  },
+  methods: {
+    submitForm() {
+      if (!this.name.trim()) {
+        this.error = 'Nama tidak boleh kosong!'
+      } else {
+        this.error = ''
+        this.$emit('add-participant', this.name.trim())
+        this.name = ''
+      }
+    }
+  }
+}
+</script>
---
+++ src/components/ParticipantList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2 class="list-title">Daftar Peserta</h2>
+    <transition-group name="fade" tag="ul" class="participant-list">
+      <li v-for="(name, index) in participants" :key="name" class="participant-item">
+        {{ name }}
+        <button @click="$emit('remove', index)" class="remove-btn">x</button>
+      </li>
+    </transition-group>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ParticipantList',
+  props: ['participants']
+}
+</script>
